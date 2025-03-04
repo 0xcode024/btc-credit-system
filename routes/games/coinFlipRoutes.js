5df15f794272fdb9e7ad57b0b74ee686b49b98ca
0xcode024 <smart.code.tec024@gmail.com>
2025-03-05 05:43:59 +0900

    add the middleware to the coinflip api

--- routes/games/coinFlipRoutes.js
+++ routes/games/coinFlipRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const coinFlipController = require("../../controllers/games/coinflip");
-
-router.post("/play", coinFlipController.playCoinFlip);
-router.get("/history/:userAddress", coinFlipController.getHistory);
+const { authenticate } = require("../../middlewares/auth");
+router.post("/play", authenticate, coinFlipController.playCoinFlip);
+router.get(
+  "/history/:userAddress",
+  authenticate,
+  coinFlipController.getHistory
+);
 
 module.exports = router;
