de3547df5eed4a5e2d411efd2d03da721b6f0d80
0xcode024 <smart.code.tec024@gmail.com>
2025-04-07 19:57:42 +0900

    update the user routes

--- routes/user.js
+++ routes/user.js
@@ -3,15 +3,17 @@
 const express = require("express");
 const userController = require("../controllers/user");
 const { authenticate } = require("../middlewares/auth");
+const { validateUserInput } = require("../middlewares/validateUser");
 
 const router = express.Router();
 
 router.post("/", userController.createUser);
-router.get("/", authenticate, userController.getUsers);
+router.get("/all", authenticate, userController.getUsers);
 router.get("/info", authenticate, userController.getUserInfo);
-router.get("/info/:address", userController.getUserByPaymentAddress);
+router.get("/info/:address", userController.getUserByAddress);
+router.put("/info", authenticate, validateUserInput, userController.updateUser);
+router.delete("/info", authenticate, userController.deleteUser);
+
 router.get("/:id", authenticate, userController.getUserById);
-router.put("/:id", authenticate, userController.updateUser);
-router.delete("/:id", authenticate, userController.deleteUser);
 
 module.exports = router;
